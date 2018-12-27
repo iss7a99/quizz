861b17458339b55f4198332cbd36e529f7249336
= <=>
2018-12-27 20:53:32 +0200

    Tryable commit

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,39 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/**
+* root directory
+* @return text.
+*/
 Route::get('/', function () {
-    return view('welcome');
+	return 'Welcome';
 });
+
+/**
+* quizz sub routes directory
+* 
+*/
+Route::get('/quizz', 'QuizzController@index')->name('quizz');
+/*Route::get('/quizz', function (\App\Question $q) {
+	return view('test')->with('quest', $q->with('choices')->first()->toJson());
+});*/
+
+/**
+* Request to 
+*/
+Route::post('/quizz/{questId}/{choicedId}', 'QuizzController@choiceChecker');
+
+/**
+* adding question area
+*/
+
+Route::get('/quizz/create', 'QuizzController@create')->name('quizz.create');
+/**
+* change the question
+*/
+Route::post('/quizz/change/{questId}', 'QuizzController@change')->name('quizz.change');
+
+/**
+* post
+*/
+Route::post('/quizz/store', 'QuizzController@store')->name('quizz.store');
